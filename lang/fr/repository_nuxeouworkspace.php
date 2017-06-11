342fdffe3dce6245953beb0f7f41473b25635c31
valery <valery.fremaux@gmail.com>
2017-06-11 17:32:01 +0200

    first code load

---
+++ lang/fr/repository_nuxeouworkspace.php
@@ -0,0 +1,34 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'repository_nuxeo', language 'fr', branch 'MOODLE_24_STABLE'
+ *
+ * @package   repository_nuxeouworkspace
+ * @copyright 2014 Rectorat Rennes
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+$string['nuxeouworkspace:view'] = 'Voir le dépot Nuxeo';
+
+$string['username'] = 'nom d\'utilisateur';
+$string['pluginname'] = 'Dépot personnel Nuxeo';
+$string['configplugin'] = 'Configuration';
+$string['Workspaces'] = 'Workspaces';
+$string['nuxeoRoot'] = 'home';
+$string['search'] = 'Rechercher';
+$string['search_name'] = 'Nom du document';
+
+$string['configerror'] = 'Erreur: fichier de configuration non valide';
---
+++ tests/generator/lib.php
@@ -0,0 +1,49 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Alfresco repository data generator
+ *
+ * @package    repository_alfresco
+ * @category   test
+ * @copyright  2013 Frédéric Massart
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+/**
+ * Alfresco repository data generator class
+ *
+ * @package    repository_nuxeo
+ * @category   test
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class repository_nuxeo_generator extends testing_repository_generator {
+
+    /**
+     * Fill in record defaults.
+     *
+     * @param array $record
+     * @return array
+     */
+    protected function prepare_record(array $record) {
+        $record = parent::prepare_record($record);
+        if (!isset($record['nuxeo_url'])) {
+            $record['nuxeo_url'] = 'http://no.where.com/nuxeo/api';
+        }
+        return $record;
+    }
+
+}
